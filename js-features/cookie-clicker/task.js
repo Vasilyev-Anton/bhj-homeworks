3a240dbcaa78854a8b848ef62615686c3cab8868
Anton Vasilyev <vasilyev1812@gmail.com>
2022-09-17 21:20:52 +0300

    внес небольшие правки

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,6 +1,5 @@
 const img = document.getElementById("cookie"),
     count = document.getElementById("clicker__counter");
 img.onclick = () => {
-    +count.textContent++;
-    count.textContent % 2 !== 0 ? img.width +=20 : img.width -=20;
+    img.width = ++count.textContent % 2 ? 250 : 200;
 };
--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -8,8 +8,16 @@ for (let holeNumber = 1; holeNumber < 10; holeNumber++) {
     let hole = holes(holeNumber)
     hole.onclick = () => {
         hole.classList.contains("hole_has-mole") ? hit++ : miss++;
-        hit === 10 ? (alert('Вы победили!'), hit = 0, miss = 0) : '';
-        miss === 5 ? (alert('Вы проиграли!'), hit = 0, miss = 0) : '';
+        if (hit === 10) {
+            alert('Вы победили!'), 
+            hit = 0, 
+            miss = 0;
+        };
+        if (miss === 5) {
+            alert('Вы проиграли!'), 
+            hit = 0, 
+            miss = 0;
+        };
 
         getKillNumber.textContent = hit;
         getLostNumber.textContent = miss;
